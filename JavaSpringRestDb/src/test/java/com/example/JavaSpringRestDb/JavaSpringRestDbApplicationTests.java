e958c9e0fd60e18c8be370b897c4b2029c2e3526
blad1892 <bladimirrv1892@hotmail.com>
2022-09-20 16:37:08 -0400

    Agregando projec JavaSpringRestDb

---
+++ JavaSpringRestDb/src/test/java/com/example/JavaSpringRestDb/JavaSpringRestDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.JavaSpringRestDb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringRestDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
